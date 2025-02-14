eda4315adf51a010df8845a760dfecd3bf6aa62f
rakaso598 <nexonaddress98@gmail.com>
2025-02-14 18:00:30 +0900

    프로젝트 초기화 and 컴포넌트 2개 구현

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+import MyButton from './components/MyButton';
+import TextChanger from './components/TextChanger';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <MyButton />
+      <TextChanger />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/MyButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// 함수형 컴포넌트 타입 정의 (React.FC)
+const MyButton: React.FC = () => {
+    const handleClick = () => {
+        alert('Button clicked!');
+    };
+
+    return (
+        <button onClick={handleClick}>Click me</button>
+    );
+};
+
+// export default
+export default MyButton;
