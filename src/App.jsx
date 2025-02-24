fa401e4e6d32db5ada8baa9a4a0618b3bf37d135
rakaso598 <nexonaddress98@gmail.com>
2025-02-24 18:39:02 +0900

    feat: Added Hello Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { Hello } from "./Hello.jsx";
+
 function App() {
-  return <div>divdivdiv</div>;
+  const handleClick = () => {
+    alert(`안녕! 안녕!`);
+  };
+  return (
+    <>
+      <button onClick={handleClick}>안녕버튼</button>
+      <Hello />
+      <Hello />
+      <Hello />
+    </>
+  );
 }
 
 export default App;
+
+// 컴포넌트:
+// 반드시 대문자로 시작되어야 한다.
+// 항상 jsx (리액트 엘리먼트)가 return되어야 한다.
+// 리액트 엘리먼트는 HTML 태그형식이거나 컴포넌트이다.
